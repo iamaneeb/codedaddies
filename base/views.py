b04411061f7d81bf9f25808f1afcef985b70b257
Aneeb <aneebpaloth74@gmail.com>
2023-07-19 19:55:45 -0700

    finshed with frontend

--- base/views.py
+++ base/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
 
 # Create your views here.
 def home(request):
     return render(request , 'base/index.html')
+
+def search(request):
+    search_values = request.POST.get('search')
+    front_end_stuff = {
+        'search' : search_values
+    }
+    return render(request, 'base/search.html',front_end_stuff)
